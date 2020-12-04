7c2ef6174ec3eb89a5396f6254751d85521d8a7a
thesebonesalone <duikhead2@gmail.com>
2020-12-04 14:08:53 -0600

    Next commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+    def index
+        render index.erb
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
   resources :users
+  resources :welcome
 
   get '/users/:username/notes', to: 'users#getnotes'
   post '/auth', to: 'auth#create'
